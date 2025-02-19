37011c6772e2f0280783e9690997e61b00ca7d9a
Pranav Tartey <pranavtartey@gmail.com>
2025-02-19 15:49:07 +0530

    support of postgres added correctly

--- apps/main/src/controller/index.ts
+++ apps/main/src/controller/index.ts
@@ -1,11 +1,21 @@
+import { CreateSchema } from "@repo/common/types";
 import { prisma } from "@repo/userdb/user";
 import { Request, Response } from "express";
 
-
 export const signin = async (req: Request, res: Response) => {
     try {
+
+        const parsedData = CreateSchema.safeParse(req.body);
+        if (!parsedData.success) {
+            res.send({
+                success: parsedData.success,
+                message: "something went wrong in the signin controller"
+            })
+            return
+        }
+
         const user = await prisma.user.create({
-            data: req.body
+            data: parsedData.data
         })
 
         console.log(user)
@@ -14,5 +24,8 @@ export const signin = async (req: Request, res: Response) => {
 
     } catch (e) {
         console.error("Something went wrong in the singin controller")
+        res.send({
+            message: "Something went wrong in the singin controller"
+        })
     }
 }
