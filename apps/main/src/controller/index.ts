64f8a169fe4189fe3de2b4129963273e317d5693
Pranav Tartey <pranavtartey@gmail.com>
2025-02-19 14:40:48 +0530

    added the support for postgres using prisma

--- apps/main/src/controller/index.ts
+++ apps/main/src/controller/index.ts
@@ -1,5 +1,18 @@
+import { prisma } from "@repo/userdb/user";
 import { Request, Response } from "express";
 
-export const signin = (req: Request, res: Response) => {
-    res.send("This app is workin fine and you have signed up")
+
+export const signin = async (req: Request, res: Response) => {
+    try {
+        const user = await prisma.user.create({
+            data: req.body
+        })
+
+        console.log(user)
+
+        res.send(`The user was created : ${user}`)
+
+    } catch (e) {
+        console.error("Something went wrong in the singin controller")
+    }
 }
